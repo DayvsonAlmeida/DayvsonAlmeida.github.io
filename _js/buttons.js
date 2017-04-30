417d040316f68cec62c91ed27bfdeb15ff7b3d44
Dayvson Almeida <dayvsonwilkson@gmail.com>
2017-04-30 00:33:37 -0300

    Alterações de apresentação

--- _js/buttons.js
+++ _js/buttons.js
@@ -2,7 +2,7 @@ function button_action(id){
    var value;
    if(document.getElementById(id).innerHTML == "true"){
       document.getElementById(id).innerHTML = "false";
-      document.getElementById(id).style.backgroundColor = '#FF041E';
+      document.getElementById(id).style.backgroundColor = '#FF0000';
       value = false;
    }else {
       document.getElementById(id).innerHTML = "true";
@@ -10,4 +10,19 @@ function button_action(id){
       value = true;
    }
    return value;
+}
+function button_mouseover(id){
+   if(document.getElementById(id).innerHTML == "true"){
+      document.getElementById(id).style.backgroundColor = "#3e8e41";
+   }else{
+      document.getElementById(id).style.backgroundColor = '#800000';
+   }
+}
+
+function button_mouseout(id){
+   if(document.getElementById(id).innerHTML == "true"){
+      document.getElementById(id).style.backgroundColor = "#4CAF50";
+   }else{
+      document.getElementById(id).style.backgroundColor = '#FF0000';
+   }
 }
